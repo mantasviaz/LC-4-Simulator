ca8dcf55e67cafcd0945f72f37cbd28703a07104
Mantas Viazmitinas <158117948+mantasviaz@users.noreply.github.com>
2024-06-19 13:24:24 -0400

    Update lc4_disassembler.c

--- lc4_disassembler.c
+++ lc4_disassembler.c
@@ -1,11 +1,3 @@
-/************************************************************************/
-/* File Name : lc4_disassembler.c 										*/
-/* Purpose   : This file implements the reverse assembler 				*/
-/*             for LC4 assembly.  It will be called by main()			*/
-/*             															*/
-/* Author(s) : tjf and you												*/
-/************************************************************************/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include "lc4_hash.h"
