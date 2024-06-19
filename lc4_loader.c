d4364b46cfa207928836ca32bc7e99294bffd058
Mantas Viazmitinas <158117948+mantasviaz@users.noreply.github.com>
2024-06-19 13:25:38 -0400

    Update lc4_loader.c

--- lc4_loader.c
+++ lc4_loader.c
@@ -1,11 +1,3 @@
-/************************************************************************/
-/* File Name : lc4_loader.c		 										*/
-/* Purpose   : This file implements the loader (ld) from PennSim		*/
-/*             It will be called by main()								*/
-/*             															*/
-/* Author(s) : tjf and you												*/
-/************************************************************************/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include "lc4_memory.h"
